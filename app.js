8223b71cb66ab6b00f4ca0a115ec19058491eeb7
thayllatai <thayllateixeirapaz3@gmail.com>
2025-02-03 19:50:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 4');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
 exibirMensagemInicial();
